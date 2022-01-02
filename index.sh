3f054cd51a7fe331e5bb1c08f5e9340faaefbb71
Lorenzo Fontoura <lorenzo@nioel.com>
2022-01-02 12:31:06 +1000

    move to fedora

--- index.sh
+++ index.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 i() {
-  sudo pacman -S --noconfirm --needed $1
+  sudo dnf install -y $1
 }
 is_installed() {
   if !(command -v $1 &> /dev/null); then
     return 1;
   fi
   return 0;
-}
-i_aur() {
-  if (is_installed $2); then return 1; fi;
-  git clone https://aur.archlinux.org/$1.git
-  cd $1
-  makepkg -si
-  cd ..
-  rm -rf $1
 }
 i_snap() {
   if (is_installed $1); then return 1; fi;
@@ -25,40 +17,18 @@ preconfig() {
 }
 install() {
   # Fundamentals
-  i "base-devel"
-  i "wget"
-  i "openssh"
-  i "vim"
-  i "git"
-  i "fuse" # Run AppImages.
-  # Graphics
-  i "nvidia"
-  i "xorg-server"
-  i "xorg-apps"
-  i "xorg-xinit"
-  # Desktop Environment
-  i "plasma"
-  i "konsole"
-  i "spectacle" # Screenshots.
-  i "libgtk-3.so"
-  # Generic dependencies
-  i "lib32-gcc-libs"
-  i "libpng12"
-  i "libpqxx"
+  i "make"
+  i "automake"
+  i "gcc"
+  i "gcc-c++"
+  i "kernel-devel"
   i "npm"
-  i "python2"
-  i_aur "gconf" "gconf"
-  # Applications
-  i "gimp"
-  if !(is_installed "snap"); then
-    i_aur "snapd" "snap"
-    sudo systemctl enable --now snapd.socket
-    sudo ln -s /var/lib/snapd/snap /snap
-  fi
+  i "snapd"
+  sudo systemctl enable --now snapd.socket
+  sudo ln -s /var/lib/snapd/snap /snap
 }
 install_snaps() {
   # Basics
-  i_snap "chromium"
   i_snap "discord"
   i_snap "spotify"
   # Development
@@ -67,14 +37,7 @@ install_snaps() {
   i_snap "clion" "--classic"
 }
 postconfig() {
-  sudo systemctl enable sddm.service
-  sudo systemctl enable dhcpcd
-  # Start X
-  if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -le 3 ]; then
-    exec startx
-  fi
-  # Start Snapd
-  sudo systemctl start snapd.service
+  echo "done"
 }
 preconfig
 install
