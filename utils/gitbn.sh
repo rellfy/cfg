970ce825cd87db961a1c6045e3a921ea69589d42
Lorenzo Fontoura <lorenzo@nioel.com>
2023-05-19 13:51:14 +1000

    add comment

--- utils/gitbn.sh
+++ utils/gitbn.sh
@@ -3,5 +3,6 @@ set -e
 fg_green="\033[0;32m";
 fg_reset="\033[0m";
 current_branch_name=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD);
+# Print the current branch name.
 echo -e "$fg_green$current_branch_name$fg_reset";
 
